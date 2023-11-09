7d8281377dbc9681e096b7571d4570f14f32af61
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-11-09 12:14:27 +0600

    tour type category designed

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div className='w-1/3 mx-auto text-center'>
-            <h3 className='text-3xl text-blue-900 font-Handlee'>{heading}</h3>
-            <h2 className='text-5xl text-slate-600 font-bold'>{subHeading}</h2>
+        <div className='md:w-1/3 mx-auto text-center'>
+            <h3 className='text-xl md:text-3xl text-blue-900 font-Handlee'>{heading}</h3>
+            <h2 className='text-3xl md:text-5xl text-slate-600 font-bold'>{subHeading}</h2>
         </div>
     );
 };
--- src/Pages/Home/TourType/TourType.jsx
+++ src/Pages/Home/TourType/TourType.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import { FaCity, FaConciergeBell, FaHiking, FaShip, FaSuitcaseRolling, FaUmbrellaBeach } from 'react-icons/fa';
 
 const TourType = () => {
     return (
-        <div className='bg-[#ecdfdf] py-16'>
-            <SectionTitle 
-            heading='Browse By Category'
-            subHeading='Pick A Tour Type'
-            />
-        </div>
+        <section className='bg-[#ecdfdf] py-16'>
+            <div className='mt-16 md:mt-0'>
+                <SectionTitle
+                    heading='Browse By Category'
+                    subHeading='Pick A Tour Type'
+                />
+            </div>
+
+            <div className="container mt-12 grid md:grid-cols-3 lg:grid-cols-6 gap-5">
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaCity />
+                    </div>
+                    <h6 className='font-medium'>City Tour</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaUmbrellaBeach />
+                    </div>
+                    <h6 className='font-medium'>Beaches</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaShip />
+                    </div>
+                    <h6 className='font-medium'>Boat Tour</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaSuitcaseRolling />
+                    </div>
+                    <h6 className='font-medium'>Adventure</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaConciergeBell />
+                    </div>
+                    <h6 className='font-medium'>Food</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+                <div className='bg-white py-5 rounded-md flex flex-col items-center'>
+                    <div className='bg-yellow-300 p-5 rounded-full text-xl mb-3'>
+                        <FaHiking />
+                    </div>
+                    <h6 className='font-medium'>Hiking</h6>
+                    <p className='text-slate-500 text-sm'>10 Tours+</p>
+                </div>
+            </div>
+
+            <div className='container px-10 flex mt-16'>
+                <div>
+                    <img src="./images/image17.png" alt="" />
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
